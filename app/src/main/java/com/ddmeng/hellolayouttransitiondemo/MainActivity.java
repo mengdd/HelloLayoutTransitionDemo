7f50dfb41a13a50e2c03d2f8bc708abc2ce35a60
Dandan MENG <mengdandanno1@163.com>
2017-06-16 11:05:04 +0800

    use custom layout transitions

--- app/src/main/java/com/ddmeng/hellolayouttransitiondemo/MainActivity.java
+++ app/src/main/java/com/ddmeng/hellolayouttransitiondemo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.ddmeng.hellolayouttransitiondemo;
 
+import android.animation.LayoutTransition;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -11,6 +16,8 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.container)
+    ViewGroup containerView;
     @BindView(R.id.tab1)
     TextView tab1;
     @BindView(R.id.tab2)
@@ -25,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initLayoutTransition();
     }
 
     @OnClick({R.id.tab1, R.id.tab2, R.id.tab3, R.id.tab4})
@@ -38,5 +46,21 @@ public class MainActivity extends AppCompatActivity {
         tab2.setVisibility(View.VISIBLE);
         tab3.setVisibility(View.VISIBLE);
         tab4.setVisibility(View.VISIBLE);
+    }
+
+    private void initLayoutTransition(){
+        LayoutTransition layoutTransition = new LayoutTransition();
+        PropertyValuesHolder pvhLeft = PropertyValuesHolder.ofInt("left", 0, 1);
+        PropertyValuesHolder pvhRight = PropertyValuesHolder.ofInt("right", 0, 1);
+
+        ObjectAnimator changingAnimation = ObjectAnimator.ofPropertyValuesHolder(containerView,
+                pvhLeft, pvhRight);
+        changingAnimation.setInterpolator(new DecelerateInterpolator());
+
+        layoutTransition.setAnimator(LayoutTransition.CHANGE_APPEARING, changingAnimation);
+        layoutTransition.setAnimator(LayoutTransition.CHANGE_DISAPPEARING, changingAnimation);
+        layoutTransition.setStartDelay(LayoutTransition.CHANGE_DISAPPEARING, 0);
+        layoutTransition.setDuration(5000);
+        containerView.setLayoutTransition(layoutTransition);
     }
 }
